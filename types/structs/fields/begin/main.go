b4bbc68cce0c2cbb71420310e04fd8e624741f82
Peeradech <54458656+pabhi-pete@users.noreply.github.com>
2024-02-14 19:59:16 +0000

    Mod struct-field

--- types/structs/fields/begin/main.go
+++ types/structs/fields/begin/main.go
@@ -1,13 +1,25 @@
 // types/structs/fields/begin/main.go
 package main
 
+// import (
+// 	"fmt"
+// )
+import "fmt"
+
 // define a struct type for author
 //
+type author struct {
+	first, last string
+}
 
 func main() {
 	// intialize author
 	//
+	a := author{
+		first: "testfirstname",
+		last:  "testlastname",
+	}
 
 	// print the author
-	// fmt.Printf("%#v\n", a)
+	fmt.Printf("%#v\n", a)
 }
