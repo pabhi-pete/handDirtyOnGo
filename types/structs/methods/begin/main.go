99a8e71d0dbd66f17bd764b696f7186500e52561
Peeradech <54458656+pabhi-pete@users.noreply.github.com>
2024-02-14 20:31:22 +0000

    Mod structs-methods

--- types/structs/methods/begin/main.go
+++ types/structs/methods/begin/main.go
@@ -1,6 +1,8 @@
 // types/structs/methods/begin/main.go
 package main
 
+import "fmt"
+
 type author struct {
 	first string
 	last  string
@@ -8,14 +10,23 @@ type author struct {
 
 // fullName returns the full name of the author
 //
+func authorFullName(first string, last string) {
+	fmt.Println("The author's name is: ", first, last)
+}
+
+//alternate way to get author fullname
+func (a author) getFullName() string {
+	return a.first + " " + a.last
+}
 
 func main() {
 	// initialize author
-	// a := author{
-	// 	first: "Marcus",
-	// 	last:  "Aurelius",
-	// }
+	a := author{
+		first: "Marcus",
+		last:  "Aurelius",
+	}
 
 	// print the author's full name
-	//
+	authorFullName(a.first, a.last)
+	fmt.Println(a.getFullName())
 }
