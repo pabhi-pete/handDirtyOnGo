b70771166b2c4286395f85ef14c2d2cc7b510f0a
Peeradech <54458656+pabhi-pete@users.noreply.github.com>
2024-02-15 00:56:10 +0000

    Mod arrays-slice

--- types/arrays/begin/main.go
+++ types/arrays/begin/main.go
@@ -1,13 +1,15 @@
 // types/arrays/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// declare an array of integers
-
+	var arr [3]int
 	// print the array
-
+	fmt.Println(arr)
 	// set the first element to 1
-
+	arr[0] = 1
 	// print the array
-
+	fmt.Println(arr)
 }
--- types/slices/begin/main.go
+++ types/slices/begin/main.go
@@ -1,18 +1,27 @@
 // types/slices/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// declare a slice string the make function
 	//
-
+	// names := make([]string, 0)
+	//alternate syntax
+	names := []string{}
 	// append 3 names to the slice
-	//
-
+	names = append(names, "Jane")
+	names = append(names, "Mary")
+	names = append(names, "John")
 	// print the slice
-
 	// slice the slice using syntax slice[low:high]
-	// [Jane Mary]
-	// [Jane Mary]
-	// [John]
+	fmt.Println(names[:2]) // [Jane Mary]
+
+	fmt.Println(names[1:])  // [Mary John]
+	fmt.Println(names[1:3]) // [Mary John]
+	fmt.Println(names[2:])  // [John]
+	fmt.Println(names[:1])  // [Jane]
 	// [John Jane Mary]
+	fmt.Println(names[:3])
+	fmt.Println(names[:])
 }
